3650ee7df9505b854d9d17147ffd462791c4e06c
nnddss <1076645646@qq.com>
2020-12-12 00:11:53 +0800

    c0

--- src/main/java/miniplc0java/tokenizer/StringIter.java
+++ src/main/java/miniplc0java/tokenizer/StringIter.java
@@ -41,7 +41,9 @@ public class StringIter {
             return;
         }
         while (scanner.hasNext()) {
-            linesBuffer.add(scanner.nextLine() + '\n');
+            String line = scanner.nextLine();
+            linesBuffer.add(line + '\n');
+            System.out.println(line);
         }
         // todo:check read \n?
         initialized = true;
